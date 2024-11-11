dfaa22043c800ab4696fd9df6895d96a84d80eca
AdithyaDharni <dharniadithya@gmail.com>
2024-11-11 15:07:11 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,28 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    // Map of Roman numeral values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+    let result = 0;
 
-module.exports={romanToInt}
+    for (let i = 0; i < s.length; i++) {
+        const currentVal = romanMap[s[i]];
+        const nextVal = romanMap[s[i + 1]];
+
+        // If the current value is less than the next value, we subtract it (like IV, IX, etc.)
+        if (nextVal && currentVal < nextVal) {
+            result -= currentVal;
+        } else {
+            result += currentVal;
+        }
+    }
+
+    return result;
+}
